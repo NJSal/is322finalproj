4eedba99178ca5705f6b6326222840cf01184653
David Salazar <salazar.davidf@gmail.com>
2020-05-07 13:15:29 -0400

    feat: initial project set up

--- src/components/App.js
+++ src/components/App.js
@@ -2,4 +2,4 @@ import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 
-import { userChoice, userError } from "../actions"; 
+import { userChoice, userError } from "../actions";
